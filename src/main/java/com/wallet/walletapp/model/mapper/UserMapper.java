3d54d1c981476de63de8d0d86a7d932a82d11e11
ramin karimkhani <ramin.k92@gmail.com>
2024-07-22 01:14:59 +0330

    Mapping ignore

--- src/main/java/com/wallet/walletapp/model/mapper/UserMapper.java
+++ src/main/java/com/wallet/walletapp/model/mapper/UserMapper.java
@@ -3,12 +3,19 @@ package com.wallet.walletapp.model.mapper;
 import com.wallet.walletapp.model.dto.UserDto;
 import com.wallet.walletapp.model.entity.AppUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toDto(AppUser entity);
 
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "enabled", ignore = true),
+            @Mapping(target = "roles", ignore = true)
+    })
     AppUser toEntity(UserDto dto);
 
 }
