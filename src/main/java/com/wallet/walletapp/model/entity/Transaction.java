e26188ebc1f3f8b739f8492198a0d28472a1a6b7
ramin karimkhani <ramin.k92@gmail.com>
2024-07-22 22:30:13 +0330

    create Wallet, Transaction Entity

---
+++ src/main/java/com/wallet/walletapp/model/entity/Transaction.java
@@ -0,0 +1,36 @@
+package com.wallet.walletapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime insertTime;
+
+}
---
+++ src/main/java/com/wallet/walletapp/model/entity/Wallet.java
@@ -0,0 +1,46 @@
+package com.wallet.walletapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "wallet")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Person user;
+
+    @OneToMany(mappedBy = "wallet", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Transaction> transactionList;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime insertTime;
+
+}
