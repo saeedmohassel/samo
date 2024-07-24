6d405553cb408244dd40ab65be1e1a1268571d14
ramin karimkhani <ramin.k92@gmail.com>
2024-07-24 11:44:42 +0330

    UserController Api Documentation

--- src/main/java/com/wallet/walletapp/controller/UserController.java
+++ src/main/java/com/wallet/walletapp/controller/UserController.java
@@ -6,6 +6,8 @@ import com.wallet.walletapp.security.UserPrincipal;
 import com.wallet.walletapp.service.PersonService;
 import com.wallet.walletapp.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -38,7 +40,11 @@ public class UserController {
 
     @Operation(
             summary = "Register User",
-            description = "Register new User and returns an access token"
+            description = "Register new User and returns an access token",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "UserDto object for creating a user",
+                    content = @Content(schema = @Schema(implementation = UserDto.class))
+            )
     )
     @ApiResponses(
             value = {
@@ -68,7 +74,9 @@ public class UserController {
     )
     @GetMapping("/{username}")
     @PreAuthorize("principal.username == #username")
-    public ResponseEntity<UserDto> getUserByUsername(@PathVariable String username) {
+    public ResponseEntity<UserDto> getUserByUsername(
+            @Parameter(description = "username of the user to be retrieved", required = true)
+            @PathVariable String username) {
         log.info("user info requester: '{}'", username);
         UserDto user = userService.findUserByUsername(username);
         return ResponseEntity.ok(user);
@@ -76,7 +84,11 @@ public class UserController {
 
     @Operation(
             summary = "User Login",
-            description = "Authenticates the user and returns an access token"
+            description = "Authenticates the user and returns an access token",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "TokenRequest object for creating a token",
+                    content = @Content(schema = @Schema(implementation = TokenRequest.class))
+            )
     )
     @ApiResponses(
             value = {
@@ -92,9 +104,26 @@ public class UserController {
         return ResponseEntity.ok(tokenResponse);
     }
 
+    @Operation(
+            summary = "Register Profile",
+            description = "Users should create a profile to work with the application",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Person object for creating a profile",
+                    content = @Content(schema = @Schema(implementation = PersonRequestDto.class))
+            )
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Profile created successfully",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = PersonDto.class))}),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "User Not Found")
+            }
+    )
     @PostMapping("/{username}/profile")
     @PreAuthorize("principal.username == #username")
     public ResponseEntity<PersonDto> registerProfile(@Valid @RequestBody PersonRequestDto personRequest,
+                                                     @Parameter(description = "Username of the profile to be created", required = true)
                                                      @PathVariable String username) {
         log.info("user register requester: '{}'", (
                 (UserPrincipal) SecurityContextHolder.getContext()
@@ -103,9 +132,23 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(personDto);
     }
 
+    @Operation(
+            summary = "Get User Profile",
+            description = "load user profile information"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Profile loaded successfully",
+                            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TokenResponse.class))}),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized"),
+                    @ApiResponse(responseCode = "404", description = "User Not Found")
+            }
+    )
     @GetMapping("/{username}/profile")
     @PreAuthorize("principal.username == #username")
-    public ResponseEntity<PersonDto> getProfile(@PathVariable String username) {
+    public ResponseEntity<PersonDto> getProfile(
+            @Parameter(description = "username of the profile to be retrieved", required = true)
+            @PathVariable String username) {
         log.info("user profile requester: '{}' resource address: '{}'",
                 ((UserPrincipal) SecurityContextHolder.getContext()
                         .getAuthentication().getPrincipal()).getUsername(), username);
@@ -113,6 +156,19 @@ public class UserController {
         return ResponseEntity.ok(personDto);
     }
 
+    @Operation(
+            summary = "Get Gender List",
+            description = "load gender list of persons"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "gender list loaded successfully",
+                            content = {
+                                    @Content(mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = String.class)))
+                            })
+            }
+    )
     @GetMapping("/genders")
     public ResponseEntity<List<String>> getGenderList() {
         log.info("user register requester: '{}'", (
@@ -120,6 +176,5 @@ public class UserController {
                         .getAuthentication().getPrincipal()).getUsername());
         return ResponseEntity.status(HttpStatus.OK).body(userService.getGenders());
     }
-
 
 }
